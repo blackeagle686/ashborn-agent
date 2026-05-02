cff8ecd33e37cb832be9d119d886783e7a61f920
blackeagle686 <mathematecs1@gmail.com>
2026-05-02 19:00:16 +0300

    Update VS Code theme names in panel.js

--- vscode-extension/out/panel.js
+++ vscode-extension/out/panel.js
@@ -128,7 +128,7 @@ class AshbornViewProvider {
                 break;
             case "theme":
                 const workbenchConfig = vscode.workspace.getConfiguration("workbench");
-                const theme = msg.isLight ? "Default Light Modern" : "Default Dark Modern";
+                const theme = msg.isLight ? "Default Light+" : "Default Dark+";
                 await workbenchConfig.update("colorTheme", theme, vscode.ConfigurationTarget.Global);
                 break;
         }
