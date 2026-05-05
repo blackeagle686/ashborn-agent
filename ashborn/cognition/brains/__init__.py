d72e09b8060154acbedef386f4b9731e95c0bdac
blackeagle686 <mathematecs1@gmail.com>
2026-05-05 17:48:00 +0300

    Added Ashborn cognition brain modules

---
+++ ashborn/cognition/brains/__init__.py
@@ -0,0 +1,6 @@
+from .thinker import AshbornThinker
+from .planner import AshbornPlanner
+from .reflector import AshbornReflector
+from .generator import AshbornGenerator
+
+__all__ = ["AshbornThinker", "AshbornPlanner", "AshbornReflector", "AshbornGenerator"]
